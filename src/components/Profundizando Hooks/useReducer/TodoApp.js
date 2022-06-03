e55f4967f8ad3791c6b21db9393d81bb453ae615
andersonNB <andersonaugustonb@ufps.edu.co>
2022-06-02 22:42:46 -0500

    Agregando en el localStorage los todos y utilizando la función init del useReducer

--- src/components/Profundizando Hooks/useReducer/TodoApp.js
+++ src/components/Profundizando Hooks/useReducer/TodoApp.js
@@ -1,21 +1,59 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { todoReducer } from './todoReducer'
-import { Button, Row, Col, Form, Input, } from 'antd';
+import { Button, Row, Col, Form, Input, Space } from 'antd';
 
 //estado inicial
-const initialState = [{
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false,
-}]
+// const initialState = [{
+//     id: new Date().getTime(),
+//     desc: 'Aprender React',
+//     done: false,
+// }]
+
+//Un función que cuando retorne algo sera el valor del initial State
+const init = () => {
+    return [{
+        id: new Date().getTime(),
+        desc: 'Aprender React',
+        done: false,
+    }]
+}
 
 
 const TodoApp = () => {
 
     //Reducer como tal y nuestro listado inicial
-    const [todo] = useReducer(todoReducer, initialState)
+    const [todo, dispatch] = useReducer(todoReducer, [], init)
 
     console.log("todo  ", todo)
+
+    //useEffect pendiente de los cambios de los todos
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todo))
+    }, [todo])
+
+    const handleSubmit = (data) => {
+
+        if (data.inputTODO.length !== 0) {
+            //crear el todo o el objeto como tal y por aparte su acción
+            const newTodo = {
+                id: new Date().getTime(),
+                desc: data.inputTODO,
+                done: false,
+            }
+
+            //Se pasa la acción a la función dispatch
+            const action = {
+                type: 'add',
+                payload: newTodo,
+            }
+
+            dispatch(action)
+            //Limpiar el input
+
+        }
+    }
+
+
     return (
         <div>
             <hr />
@@ -28,21 +66,25 @@ const TodoApp = () => {
                         {
                             todo.map((todo, i) => {
 
-                                return <li key={ todo.id }>{ i + 1 }. { todo.desc }
+                                return (<li key={ todo.id } style={ { listStyle: 'none', margin: '1rem', backgroundColor: '#FFF2F2' } }>{ i + 1 }. { todo.desc }
+                                    <br />
                                     <Button type="primary" danger={ true }> Borrar</Button>
-                                </li>
+                                </li>)
 
                             })
                         }
                     </ul>
                 </Col>
                 <Col md={ 12 } style={ { backgroundColor: "yellow" } } >
-                    <Form>
+                    <Form onFinish={ handleSubmit }>
                         <Form.Item>
-                            <Input placeholder="Aprender"></Input>
+                            Agregar TODO
+                        </Form.Item>
+                        <Form.Item name="inputTODO">
+                            <Input name="i" placeholder="Aprender"></Input>
                         </Form.Item>
                         <Form.Item>
-                            <Button>
+                            <Button htmlType="submit">
                                 Agregar
                             </Button>
                         </Form.Item>
